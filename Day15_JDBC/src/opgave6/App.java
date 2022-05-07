0bd7f87052de002694088278c4e8f5d16c24b80b
luka2477 <luke.da.duke@hotmail.com>
2022-05-07 17:47:52 +0200

    Day15 Opgave 6

--- Day15_JDBC/src/opgave6/App.java
+++ Day15_JDBC/src/opgave6/App.java
@@ -1,6 +1,35 @@
 package opgave6;
 
+import java.util.ArrayList;
+
 public class App {
+    public static void main(String[] args) {
+        final int NUMBER_COUNT = 7;
+        final int MAX_NUMBER = 36;
+        final int ROW_COUNT = 10000000;
 
+        Lotto lotto = new Lotto(NUMBER_COUNT, MAX_NUMBER, ROW_COUNT);
 
+        long start = System.nanoTime();
+        lotto.createRows();
+        long stop = System.nanoTime();
+        System.out.printf("Det tog %fms at generere %,d rækker.%n", (stop - start) / 1000000.0, ROW_COUNT);
+
+        start = System.nanoTime();
+        lotto.createSelection();
+        stop = System.nanoTime();
+        System.out.printf("Det tog %fms at generere %,d træknumre.%n", (stop - start) / 1000000.0, NUMBER_COUNT);
+
+        start = System.nanoTime();
+        lotto.createStatistics();
+        stop = System.nanoTime();
+        System.out.printf("Det tog %fms at generere statistik på %,d rækker.%n", (stop - start) / 1000000.0, ROW_COUNT);
+
+        System.out.printf("Træknumrene er: %s%n", lotto.getSelection());
+        ArrayList<Integer> statistics = lotto.getStatistics();
+        for (int i = 0; i < statistics.size(); i++) {
+            System.out.printf("%3d rigtige: %,d%n", i, statistics.get(i));
+        }
+        System.out.printf("Samlede kontrollerede rækker: %,d", statistics.stream().mapToInt(n -> n).sum());
+    }
 }
--- Day15_JDBC/src/opgave6/Lotto.java
+++ Day15_JDBC/src/opgave6/Lotto.java
@@ -1,23 +1,73 @@
 package opgave6;
 
+import java.util.ArrayList;
+
 public class Lotto {
-
+    private final int NUMBER_COUNT;
     private final int MAX_NUMBER;
-    private final int ROWS;
+    private final int ROW_COUNT;
 
-    private final int[][] note;
+    private final ArrayList<ArrayList<Integer>> rows;
+    private final ArrayList<Integer> selection;
+    private final ArrayList<Integer> statistics;
 
-    public Lotto(int maxNumber, int rows) {
-        MAX_NUMBER = maxNumber;
-        ROWS = rows;
+    public Lotto(int numberCount, int maxNumber, int rowCount) {
+        NUMBER_COUNT = numberCount;
+        MAX_NUMBER = maxNumber + 1;
+        ROW_COUNT = rowCount;
 
-        note = new int[ROWS][MAX_NUMBER];
-
-        create();
+        rows = new ArrayList<>(ROW_COUNT);
+        selection = new ArrayList<>(NUMBER_COUNT);
+        statistics = new ArrayList<>(NUMBER_COUNT + 1);
     }
 
-    public void create() {
+    public void createRows() {
+        for (int i = 0; i < ROW_COUNT; i++) {
+            rows.add(new ArrayList<>(MAX_NUMBER));
 
+            for (int j = 0; j < NUMBER_COUNT; j++) {
+                rows.get(i).add(randGen(i));
+            }
+        }
     }
 
+    public void createSelection() {
+        for (int i = 0; i < NUMBER_COUNT; i++) {
+            selection.add(randGen(i));
+        }
+    }
+
+    public ArrayList<Integer> getSelection() {
+        return new ArrayList<>(selection);
+    }
+
+    public void createStatistics() {
+        for (int i = 0; i < NUMBER_COUNT + 1; i++) {
+            statistics.add(0);
+        }
+
+        for (ArrayList<Integer> arr : rows) {
+            int count = 0;
+
+            for (int num : arr) {
+                if (selection.contains(num)) {
+                    count++;
+                }
+            }
+
+            statistics.set(count, statistics.get(count) + 1);
+        }
+    }
+
+    public ArrayList<Integer> getStatistics() {
+        return new ArrayList<>(statistics);
+    }
+
+    private int randGen(int i) {
+        int rand = 0;
+        while (rand == 0 || rows.get(i).contains(rand)) {
+            rand = Math.max((int) Math.ceil(Math.random() * MAX_NUMBER), 1);
+        }
+        return rand;
+    }
 }
